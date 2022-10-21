1d92cdb27c0cb9799be12f4c9280853e2b2b67b9
MinhPhuong-2104 <116358834+MinhPhuong-2104@users.noreply.github.com>
2022-10-21 19:26:16 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+   <a href="home.php" class="logo">NATAtravel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">Trang chủ</a>
+      <a href="about.php">Blog</a>
+      <a href="book.php">Đặt tour</a>
+      <a href="package.php">Đánh giá</a>
+   
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home">
+
+   <div class="swiper home-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide" style="background:url(hinh-anh-nha-trang-7.jpg) no-repeat">
+            <div class="content">
+               <span>Khám phá, trải nghiệm, du lịch</span>
+               <h3>Tận hưởng kì nghỉ của bạn</h3>
+               <a href="package.php" class="btn">Khám phá thêm</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(hinh-anh-nha-trang-12.jpg) no-repeat">
+            <div class="content">
+               <span>Khám phá, trải nghiệm, du lịch</span>
+               <h3>Chạm đến trái tim!</h3>
+               <a href="package.php" class="btn">Khám phá thêm</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(hinh-anh-nha-trang-13.jpg) no-repeat">
+            <div class="content">
+               <span>Khám phá, trải nghiệm, du lịch</span>
+               <h3>thiên đường du lịch biển đảo</h3>
+               <a href="package.php" class="btn">Khám phá thêm</a>
+            </div>
+         </div>
+         
+      </div>
+
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+
+   </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- services section starts  -->
+
+<section class="services">
+
+   <h1 class="heading-title"> Dịch vụ của chúng tôi </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="chill.png" alt="">
+         <h3>Thư giãn</h3>
+      </div>
+
+      <div class="box">
+         <img src="dive.png" alt="">
+         <h3>Lặn</h3>
+      </div>
+
+      <div class="box">
+         <img src="nhảy dù.png" alt="">
+         <h3>Nhảy dù</h3>
+      </div>
+
+      <div class="box">
+         <img src="swim.png" alt="">
+         <h3>Bơi</h3>
+      </div>
+
+      <div class="box">
+         <img src="icon-6.png" alt="">
+         <h3>Cắm trại</h3>
+      </div>
+
+      <div class="box">
+         <img src="icon-4.png" alt="">
+         <h3>Lửa trại</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- home about section starts  -->
+
+<section class="home-about">
+
+   <div class="image">
+      <img src="12.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>Nha Trang Blog</h3>
+      <p>Nha Trang is a great destination for those who want go to the sea for leisure travel. But do you really know how to have a trip which is worth the money?</p>
+      <a href="https://livingnomads.com/2016/03/nha-trang-blog/" class="btn">Đọc thêm</a>
+   </div>
+
+</section>
+
+<!-- home about section ends -->
+
+<!-- home packages section starts  -->
+
+<section class="home-packages">
+
+   <h1 class="heading-title"> Các tour nổi bật </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="5.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Tour 3 đảo (Vịnh San Hô - Làng Chài - Bãi Tranh)</h3>
+            <p>Là tour có giá tốt nhất Nha Trang nhưng vẫn đầy đủ dịch vụ và trải nghiệm</p>
+            <a href="book.php" class="btn">Đặt ngay</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="lan-bien-nha-trang.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Tour lặn biển ngắm san hô Nha Trang</h3>
+            <p>Trải nghiệm hoạt động lặn biển bình khí ngắm san hô tại Vịnh San Hô hoặc Hòn Mun</p>
+            <p></p>
+            <p></p>
+            
+            <a href="book.php" class="btn">Đặt ngay</a>
+         </div>
+      </div>
+      
+      <div class="box">
+         <div class="image">
+            <img src="38.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Tour tham quan thành phố Nha Trang</h3>
+            <p>Tham quan những địa điểm nổi tiếng nhất trong địa phận thành phố Nha Trang</p>
+            <p></p>
+            <p></p>
+            <a href="book.php" class="btn">Đặt ngay</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"> <a href="package.php" class="btn">Tìm thêm</a> </div>
+
+</section>
+
+<!-- home packages section ends -->
+
+<!-- home offer section starts  -->
+
+<section class="home-offer">
+   <div class="content">
+      <h3>Giảm giá lên đến 50%</h3>
+      <p>Sự hài lòng của quý khách là phương châm của chúng tôi!</p>
+      <a href="book.php" class="btn">Đặt ngay</a>
+   </div>
+</section>
+
+<!-- home offer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400104 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>mr. web designer</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="script.js"></script>
+
+</body>
+</html>
